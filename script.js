b777cf1f69912959fab63fa1a367704a5ed55f05
vittorguedessantos-wq <vittor.guedes.santos@escola.pr.gov.br>
2026-07-09 09:07:50 -0300

    cor

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("button")
+botao.addEventListener('click', curtir)
+
+function curtir() {
+    let curtidas = document.querySelector("span")
+    curtidas.textContent++;
+}
